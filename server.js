33af8b19e21d5db39bdeec0ef9d813dde92f3118
Scylol <djd3dobbin@gmail.com>
2017-04-25 15:32:09 -0400

    added rest/:id endpoint

--- server.js
+++ server.js
@@ -1,17 +1,35 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const { DEV } = require('./config');
-const knex = require('knex')(DEV);
+// const { DEV } = require('./config');
+// const knex = require('knex')(DEV);
+const knex = require('knex')({
+	client: 'pg',
+	connection: {
+		database: 'dev-restaurants-app'
+	},
+	debug: true
+});
 
 const app = express();
 app.use(bodyParser.json());
 
 app.get('/restaurants', (req, res) => {
 
-  knex.select('id', 'name', 'cuisine', 'borough')
+	knex.select('id', 'name', 'cuisine', 'borough')
     .from('restaurants')
     .limit(10)
+    .then(results => res.json(results));
+});
+
+app.get('/restaurants/:id', (req, res) => {
+	let {id} = req.params;
+	knex.select('restaurants.id', 'name', 'cuisine', 'borough', 'grades.id', 'grade', 'date as inspectionDate', 'score')
+    .from('restaurants')
+    .where('restaurants.id', id)
+    .innerJoin('grades', 'restaurants.id', 'grades.restaurant_id')    
+    .orderBy('date', 'desc')
+    .limit(1)
     .then(results => res.json(results));
 });
 
