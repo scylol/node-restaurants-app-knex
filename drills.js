5fd6f3c628dc68a376727198475b27f5d8464754
Scylol <djd3dobbin@gmail.com>
2017-04-25 12:25:50 -0400

    finished first 7 problems

--- drills.js
+++ drills.js
@@ -18,6 +18,42 @@ const knex = require('knex')({
 */
 
 // Sample select 
-knex.select('id', 'name', 'borough', 'cuisine')
-    .from('restaurants')
-    .then(results => console.log(results));
+// knex.select('id', 'name', 'borough', 'cuisine')
+//     .from('restaurants')
+//     .then(results => console.log(results));
+
+// knex.select()
+//     .from('restaurants')
+//     .then(results => console.log(results));
+
+// knex.select()
+//     .from('restaurants')
+//     .where('cuisine', 'Italian')
+//     .then(results => console.log(results));
+
+// knex.select('id', 'name')
+//     .from('restaurants')
+//     .where('cuisine', 'Italian')
+//     .limit(10)
+//     .then(results => console.log(results));
+
+// knex.count()
+//     .from('restaurants')
+//     .where('cuisine', 'Thai')
+//     .then(results => console.log(results));
+
+// knex.count()
+//     .from('restaurants')
+//     .where({cuisine:'Thai', address_zipcode: '11372'})
+//     .then(results => console.log(results));
+
+// knex.select('id', 'name', 'address_zipcode')
+//     .from('restaurants')
+//     .where('cuisine', 'Italian')
+//     .andWhere(function(){this.where('address_zipcode', '10012')
+//     .orWhere('address_zipcode', '10013')
+//     .orWhere('address_zipcode', '10014')})
+//     .orderBy('name', 'asc')
+//     .limit(5)
+//     .then(results => console.log(results)); 
+
