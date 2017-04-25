59e4fe805d33c6bffaf0c9d83ca29879997ce059
htwright <harrisontwright@gmail.com>
2017-04-25 14:05:32 -0500

    first drills done

--- drills.js
+++ drills.js
@@ -7,6 +7,7 @@ const knex = require('knex')({
   connection: {
     database: 'dev-restaurants-app'
   },
+  // debug: true
 });
 
 // If you're using ElephantSQL then the connection will look like this
@@ -50,10 +51,35 @@ const knex = require('knex')({
 // knex.select('id', 'name', 'address_zipcode')
 //     .from('restaurants')
 //     .where('cuisine', 'Italian')
-//     .andWhere(function(){this.where('address_zipcode', '10012')
-//     .orWhere('address_zipcode', '10013')
-//     .orWhere('address_zipcode', '10014')})
-//     .orderBy('name', 'asc')
+//     .whereIn('address_zipcode', ['10012', '10013', '10014'])
+// //SPAGHETTI ARCHIVE
+// //     .andWhere(function(){this.where('address_zipcode', '10012')
+// //     .orWhere('address_zipcode', '10013')
+// //     .orWhere('address_zipcode', '10014')})
+// //     .orderBy('name', 'asc')
 //     .limit(5)
-//     .then(results => console.log(results)); 
+//     .then(results => console.log(JSON.stringify(results, null, 2))); 
+// knex('restaurants').insert({name:'Byte Cafe', borough:'Brooklyn', cuisine:'coffee', address_building_number:'123', address_street:'Atlantic Avenue', address_zipcode:'11231'}, ['id', 'name'])
+// .then(res => console.log(res));
 
+
+// knex('restaurants').insert([
+//   {name:'Byte Cafe', borough:'Brooklyn', cuisine:'coffee', address_building_number:'123', address_street:'Atlantic Avenue', address_zipcode:'11231'},
+//   {name:'Bytes Chaffe', borough:'Brooklyn', cuisine:'coffee', address_building_number:'126', address_street:'Atlantic Street', address_zipcode:'11234'},
+//   {name:'Bits Casdfasdfe', borough:'Queens', cuisine:'coffee', address_building_number:'153', address_street:'Atlantic Boulevard', address_zipcode:'11221'}
+//   ], ['id', 'name'])
+//   .then(res => console.log(JSON.stringify(res, null, 2)));
+
+// knex('restaurants').where('nyc_restaurant_id', '30191841')
+// .update({
+//   name: 'DJ Reynolds Pub and Restaurant'
+// }).then(res => console.log(JSON.stringify(res, null, 2)));
+
+// knex('restaurants').select().where('nyc_restaurant_id', '30191841').then(res => console.log(JSON.stringify(res, null, 2)));
+
+
+// knex('grades').select().where('id', 13).then(res => console.log(JSON.stringify(res, null, 2)));
+// knex('grades').where('id', 13).del().then(res =>(null));
+// knex('grades').select().where('id', 13).then(res => console.log(JSON.stringify(res, null, 2)));
+
+// knex('restaurants').where('id', 50).del().then(res => console.log(res));
